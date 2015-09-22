b074fe1ed93a9bd4d0bc71325e801b00a83076db
Ryan Rempel <rgrempel@gmail.com>
2015-09-22 13:04:02 -0500

    Fix whitespace.

--- src/Native/Ticker.js
+++ src/Native/Ticker.js
@@ -1,21 +1,21 @@
 Elm.Native.Ticker = {};
 Elm.Native.Ticker.make = function (localRuntime) {
-	localRuntime.Native = localRuntime.Native || {};
-	localRuntime.Native.Ticker = localRuntime.Native.Ticker || {};
-	
+    localRuntime.Native = localRuntime.Native || {};
+    localRuntime.Native.Ticker = localRuntime.Native.Ticker || {};
+    
     if (localRuntime.Native.Ticker.values) {
-		return localRuntime.Native.Ticker.values;
-	}
+        return localRuntime.Native.Ticker.values;
+    }
 
-	var Task = Elm.Native.Task.make(localRuntime);
+    var Task = Elm.Native.Task.make(localRuntime);
 
     var wait = Task.asyncFunction(function(callback) {
         requestAnimationFrame(function(time) {
             callback(Task.succeed(time));
         });
     });
-	
+    
     return localRuntime.Native.Ticker.values = {
-		wait: wait
-	};
+        wait: wait
+    };
 };
